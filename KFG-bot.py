1cbc001b2331545107d8ee61fb6acc0788f2886f
Radics Baltazár <baltazar.radics@gmail.com>
2018-09-15 17:40:22 +0200

    added plot error handling

--- KFG-bot.py
+++ KFG-bot.py
@@ -481,8 +481,11 @@ async def ping(channel, args): # ping [delay]
 async def plot(channel, args): # can throw exception, unhandled for now
   await channel.trigger_typing()
   function = ''.join(args)
-  plot = run(['gnuplot', '-e', PLOT_OPTS.format(function=function)], capture_output=True).stdout
-  await channel.send("Plot of `{}`:".format(function), file=discord.File(plot, filename="plot.png"))
+  output = run(['gnuplot', '-e', PLOT_OPTS.format(function=function)], capture_output=True)
+  if output.returncode == 0:
+    await channel.send("Plot of `{}`:".format(function), file=discord.File(output.stdout, filename="plot.png"))
+  else:
+    await Util.sendError(channel, output.stderr.decode('utf-8'))
 
 
 async def mention(channel, args):
